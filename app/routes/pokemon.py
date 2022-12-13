b96906a111fa6bd4c9348424a2d13e3128867577
chrisjoonlee <christopherjoon@gmail.com>
2022-12-13 13:47:52 +0900

    Began working on the egg page. Now generating random numbers

--- app/routes/pokemon.py
+++ app/routes/pokemon.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
+from random import random
 
 from app.models import db, Pokemon, User
 
@@ -10,3 +11,13 @@ bp = Blueprint("pokemon", __name__, url_prefix="")
 @login_required
 def index():
     return render_template("pokemon.html")
+
+# POST route for /pokemon/new
+
+
+@bp.route("/new", methods=["POST"])
+@ login_required
+def new_pokemon():
+    id = int(random() * 152)
+    return f"<h1>{id}</h1>"
+    # return render_template("pokemon.html")
