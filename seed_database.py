af3ac1ffa3bdddde82a5ffbdfdcdbafbb84a7307
chrisjoonlee <christopherjoon@gmail.com>
2022-12-14 12:44:47 +0900

    Added more seed data

--- seed_database.py
+++ seed_database.py
@@ -1,6 +1,7 @@
 from app.settings import settings
+from app.models import Pokemon, User, UserPokemon
 import json
-from app.models import User, Pokemon
+from datetime import datetime
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -54,4 +55,96 @@ with app.app_context():
         pokemon_objects.append(new_pokemon)
 
     db.session.add_all(pokemon_objects)
+    db.session.commit()
+
+    # Add pokemon to users
+    pikachu = UserPokemon(user_id=1,
+                          pokemon_id=25,
+                          time_received=datetime.now(),
+                          time_hatched=datetime.now(),
+                          level=100)
+
+    bulbasaur = UserPokemon(user_id=1,
+                            pokemon_id=1,
+                            time_received=datetime.now(),
+                            time_hatched=datetime.now(),
+                            level=56)
+
+    squirtle = UserPokemon(user_id=1,
+                           pokemon_id=7,
+                           time_received=datetime.now(),
+                           time_hatched=datetime.now(),
+                           level=43)
+
+    pidgeotto = UserPokemon(user_id=1,
+                            pokemon_id=16,
+                            time_received=datetime.now(),
+                            time_hatched=datetime.now(),
+                            level=82)
+
+    butterfree = UserPokemon(user_id=1,
+                             pokemon_id=12,
+                             time_received=datetime.now(),
+                             time_hatched=datetime.now(),
+                             level=37)
+
+    charizard = UserPokemon(user_id=1,
+                            pokemon_id=6,
+                            time_received=datetime.now(),
+                            time_hatched=datetime.now(),
+                            level=98)
+
+    ash_pokemon = [pikachu, bulbasaur, squirtle,
+                   pidgeotto, butterfree, charizard]
+    db.session.add_all(ash_pokemon)
+    db.session.commit()
+
+    staryu = UserPokemon(user_id=2,
+                         pokemon_id=120,
+                         time_received=datetime.now(),
+                         time_hatched=datetime.now(),
+                         level=42)
+
+    starmie = UserPokemon(user_id=2,
+                          pokemon_id=121,
+                          time_received=datetime.now(),
+                          time_hatched=datetime.now(),
+                          level=66)
+
+    psyduck = UserPokemon(user_id=2,
+                          pokemon_id=54,
+                          time_received=datetime.now(),
+                          time_hatched=datetime.now(),
+                          level=100)
+
+    goldeen = UserPokemon(user_id=2,
+                          pokemon_id=118,
+                          time_received=datetime.now(),
+                          time_hatched=datetime.now(),
+                          level=58)
+
+    misty_pokemon = [staryu, starmie, psyduck, goldeen]
+    db.session.add_all(misty_pokemon)
+    db.session.commit()
+
+    geodude = UserPokemon(user_id=3,
+                          pokemon_id=74,
+                          time_received=datetime.now(),
+                          time_hatched=datetime.now(),
+                          level=25)
+
+    onix = UserPokemon(user_id=3,
+                       pokemon_id=95,
+                       time_received=datetime.now(),
+                       time_hatched=datetime.now(),
+                       level=100)
+
+    golbat = UserPokemon(user_id=3,
+                         pokemon_id=42,
+                         time_received=datetime.now(),
+                         time_hatched=datetime.now(),
+                         level=84)
+
+    brock_pokemon = [geodude, onix, golbat]
+    db.session.add_all(brock_pokemon)
     db.session.commit()
