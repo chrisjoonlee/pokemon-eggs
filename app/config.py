967fd86d13b762e0eea4f345e458252c30580794
chrisjoonlee <christopherjoon@gmail.com>
2022-12-14 16:36:40 +0900

    Changed db url to postgresql

--- app/config.py
+++ app/config.py
@@ -9,5 +9,5 @@ class Configuration:
     DB_USER = os.environ.get("DB_USER")
     DB_PASSWORD = os.environ.get("DB_PASSWORD")
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DB_URL") or "postgres://qxmzpaxzqmljve:d4d6cff99a51e8fd8850f3054eccc755521709d318a4430589a52ce202e136df@ec2-54-157-79-121.compute-1.amazonaws.com:5432/de7adt4c0oeav1"
+        "DB_URL") or "postgresql://qxmzpaxzqmljve:d4d6cff99a51e8fd8850f3054eccc755521709d318a4430589a52ce202e136df@ec2-54-157-79-121.compute-1.amazonaws.com:5432/de7adt4c0oeav1"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
